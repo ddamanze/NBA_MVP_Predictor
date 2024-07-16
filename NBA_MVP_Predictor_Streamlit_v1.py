0f1cfcdbc2d9b6e24802577c61ff089543792d8b
ddamanze <99048535+ddamanze@users.noreply.github.com>
2024-07-16 12:50:26 -0400

    Update NBA_MVP_Predictor_Streamlit_v1.py

--- NBA_MVP_Predictor_Streamlit_v1.py
+++ NBA_MVP_Predictor_Streamlit_v1.py
@@ -527,6 +527,7 @@ column_names = ["player", "pos", "age", "team_id", "g", "gs","mp_per_g", "fg_per
                 "fg2_per_g", "fg2a_per_g", "fg2_pct", "efg_pct", "ft_per_g", "fta_per_g", "ft_pct", "orb_per_g", "drb_per_g", "tr_per_g", "ast_per_g", "stl_per_g", "blk_per_g", #change to trb
                 "tov_per_g", "pf_per_g", "pts_per_g"]
 player_stats_df = pd.DataFrame(player_data_list)
+st.write(player_stats_df)
 player_stats_df.columns = column_names
 #print(player_stats_df)
 
