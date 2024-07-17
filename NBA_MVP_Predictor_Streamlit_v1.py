72ff75b101e22e9ad76d9fe0d2d6a777d771797c
ddamanze <99048535+ddamanze@users.noreply.github.com>
2024-07-16 23:33:16 -0400

    Update NBA_MVP_Predictor_Streamlit_v1.py

--- NBA_MVP_Predictor_Streamlit_v1.py
+++ NBA_MVP_Predictor_Streamlit_v1.py
@@ -789,26 +789,15 @@ def get_player_headshot_url(player_name):
 #time.sleep(5)
 
 #Remove spaces
-merged_df = merged_df.applymap(lambda x: x.replace(" ",""))
+#merged_df = merged_df.applymap(lambda x: x.replace(" ",""))
 
 with st.expander("2023-2024 Player Stats"):
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.write("Pts Per Game Leader")
-        ppg_leader_index = merged_df['pts_per_g'].idxmax()
-        ppg_leader = merged_df.loc[ppg_leader_index]
-        st.write(ppg_leader)
-        
-    with col2:
-        st.write("Rebs Per Game Leader")
-        reb_leader_index = merged_df['trb_per_g'].idxmax()
-        reb_leader = merged_df.loc[reb_leader_index]
-        st.write(reb_leader)
-    with col3:
-        st.write("Ast Per Game Leader")
-        ast_leader_index = merged_df['ast_per_g'].idxmax()
-        ast_leader = merged_df.loc[ast_leader_index]
-        st.write(ast_leader)
+#    col1, col2, col3 = st.columns(3)
+#    with col1:
+#        st.write("Pts Per Game Leader")
+#        ppg_leader_index = merged_df['pts_per_g'].idxmax()
+#        ppg_leader = merged_df.loc[ppg_leader_index]
+#        st.write(ppg_leader)
     
     st.write(merged_df)
 
