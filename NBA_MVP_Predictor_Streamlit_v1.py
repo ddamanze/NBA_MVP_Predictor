c0f127077a16470d954daff89b07bb62cc9edaf5
ddamanze <99048535+ddamanze@users.noreply.github.com>
2024-07-16 23:27:23 -0400

    Update NBA_MVP_Predictor_Streamlit_v1.py

--- NBA_MVP_Predictor_Streamlit_v1.py
+++ NBA_MVP_Predictor_Streamlit_v1.py
@@ -789,7 +789,7 @@ def get_player_headshot_url(player_name):
 #time.sleep(5)
 
 #Remove spaces
-merged_df = merged_df.applymap(lambda x: x.strip()
+merged_df = merged_df.applymap(lambda x: x.strip())
 
 with st.expander("2023-2024 Player Stats"):
         col1, col2, col3 = st.columns(3)
